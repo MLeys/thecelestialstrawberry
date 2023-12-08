f83eb1412520b4191440f93538ba580550784ecb
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-08 09:38:39 -0500

    Add social icons

--- src/components/FloatingActionBar/FloatingActionBar.jsx
+++ src/components/FloatingActionBar/FloatingActionBar.jsx
@@ -3,11 +3,11 @@ import theme from '../../theme';
 
 import Box from '@mui/material/Box';
 
-import { FacebookIcon } from '../../assets/icons';
+import { FacebookIcon, InstagramIcon, TikTokIcon } from '../../assets/icons';
 import FloatingActionButton from '../FloatingActionButton/FloatingActionButton';
 
 export default function FloatingActionBar() {
-  const iconSize = 50;
+  const iconSize = 24;
   const transitionTime = '0.3s'; 
   const scale = '1.5'
 
@@ -17,18 +17,22 @@ export default function FloatingActionBar() {
     <Box sx={{
       position: 'fixed', 
       bottom: '35%', 
-      right: theme.spacing(3), 
+      right: theme.spacing(0), 
       transform: 'translateY(50%)', 
       display: 'flex',
       flexDirection: 'column',
-      gap: theme.spacing(3), 
+      m: 0,
+      p:0,
+      gap: theme.spacing(0), 
       zIndex: 1201 
     }}>
       <FloatingActionButton icon={<FacebookIcon size={iconSize} color='blue'  />}/>
-      
-      <FloatingActionButton time={transitionTime} scale={scale} size={iconSize} />
+      <FloatingActionButton icon={<TikTokIcon size={iconSize} color='purple'  />}/>
+      <FloatingActionButton icon={<InstagramIcon size={iconSize} color='green'  />}/>
+
+      {/* <FloatingActionButton time={transitionTime} scale={scale} size={iconSize} />
       <FloatingActionButton time={transitionTime} scale={scale} size={iconSize}  img='Images/symbols/naturalHealing_02e.png' />
-      <FloatingActionButton time={transitionTime} scale={scale} size={iconSize} img='Images/symbols/tarotMystic_03.png' />
+      <FloatingActionButton time={transitionTime} scale={scale} size={iconSize} img='Images/symbols/tarotMystic_03.png' /> */}
     </Box>
   );
 };
