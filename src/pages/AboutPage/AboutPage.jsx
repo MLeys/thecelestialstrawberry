ff648d17cb8f7862e4da77df30de27b652271d90
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-10 15:42:12 -0500

    BUILD: finished about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -4,11 +4,9 @@ import { useNavigate } from "react-router-dom";
 import theme from '../../theme';
 import { aboutInfo } from '../../assets/aboutInfo';
 
-import Link from '@mui/material/Link'
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import Paper from '@mui/material/Paper';
 
 export default function AboutPage() {
   const navigate = useNavigate();
@@ -19,10 +17,11 @@ export default function AboutPage() {
   const body2 = aboutInfo.find(item => item.part === 'body2')?.text;
   const signature = aboutInfo.find(item => item.part === 'signature')?.text;
 
+
   return (
     <Grid container my={4} display={'flex'} justifyContent={'center'} >
-      <Grid xs={10} sm={6} md={4} bgcolor={'blueviolet'} >
-        Profile pic
+      <Grid xs={10} sm={8} md={4}>
+        <img src='/Images/avatarLaura.jpg' style={{  width:'100%' }}/>
       </Grid>
       <Grid xs={10} sm={8} md={8} p={2} pt={1}>
         <Typography display={{xs: 'block', sm: 'flex'}} variant='h6' pb={2} >
@@ -37,16 +36,12 @@ export default function AboutPage() {
         <Typography display={{xs: 'block', sm: 'flex'}} variant='body1' pb={1}>
           {body2}
         </Typography>
-        <Typography display={{xs: 'block', sm: 'flex'}} variant='body1' pb={1}>
+        <Typography display={{xs: 'block', sm: 'flex'}} variant='subtitle2' pb={1}>
           {signature}
         </Typography>
-
-        <Link  onClick={() => navigate('/')}>
-          <Typography  fontWeight={700}>
-            Back to Home
-          </Typography>`
-        </Link>
-
+        <Button variant='contained' color='secondary' onClick={() => navigate('/')}>
+          Back to Home
+        </Button>
       </Grid>
     </Grid>
   )
