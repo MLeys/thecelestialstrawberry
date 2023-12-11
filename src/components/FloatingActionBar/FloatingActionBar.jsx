c05f27ae1ae0673703983d3e0ec476bf18dfd4fe
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-11 11:35:07 -0500

    FIX: images displaying on deployed site

--- src/components/FloatingActionBar/FloatingActionBar.jsx
+++ src/components/FloatingActionBar/FloatingActionBar.jsx
@@ -5,13 +5,16 @@ import Box from '@mui/material/Box';
 
 import FloatingActionButton from '../FloatingActionButton/FloatingActionButton';
 
+import facebookImport from '../../assets/logos/facebook.png';
+import instagramImport from '../../Images/logos/instagram.png';
+
 const iconSize = 32;
 const transitionTime = '0.3s'; 
 const scale = '1.5';
 
-const facebookLogo = 'Images/logos/facebook.png';
-const instragramLogo ='Images/logos/instagram.png';
-const tiktokLogo = 'Images/logos/tiktok.png';
+const facebookLogo = `${facebookImport}/?raw=true`;
+const instragramLogo =`${instagramImport}/?raw=true`;
+const tiktokLogo = './thecelestialstrawberry/Images/logos/tiktok.png';
 
 const facebookLink = 'https://www.facebook.com/thecelestialstrawberry';
 const instagramLink = 'https://www.instagram.com/thecelestialstrawberry';
