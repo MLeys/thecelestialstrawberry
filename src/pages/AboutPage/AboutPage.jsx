c87b5ac019bc5e555ae05e3b2fba6b67d6aecf6c
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-11 16:05:34 -0500

    DEPLOY: fixed image path about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 import { aboutInfo } from '../../assets/aboutInfo';
-
 import { astrologyInfo } from '../../assets/servicesInformation';
+import { rootPath } from '../../assets/rootPath';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 import ServiceDetails from '../../components/ServiceDetails/ServiceDetails';
+
+const profileImage = `${rootPath}/avatarLaura.jpg`;
 
 export default function AboutPage() {
   const navigate = useNavigate();
@@ -23,7 +25,7 @@ export default function AboutPage() {
   return (
     <Grid container my={{xs: 6, md: 8}} display={'flex'} justifyContent={'center'} >
       <Grid xs={10} sm={6} md={4}>
-        <img src='/Images/avatarLaura.jpg' style={{  width:'100%' }}/>
+        <img src={profileImage} style={{  width:'100%' }}/>
       </Grid>
       <Grid xs={10} sm={8} md={8} p={2} pt={1}>
         <Typography display={{xs: 'block', sm: 'flex'}} variant='h6' pb={2} >
