0a6f30c278b130f1c386177ae5a8fa02addec7b7
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-11 17:33:54 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  plugins: [react()],
+
+  
   base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000'
+      }
+    }
+  },
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
 });
