15875d8e2fc5312fd60111b238799ad2b9b8dc10
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-13 21:19:01 -0500

    BUILD: services page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import IntroSection from './sections/IntroSection';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Layout from './pages/Layout/Layout';
 import AboutPage from './pages/AboutPage/AboutPage';
+import ServicesPage from './pages/ServicesPage/ServicesPage';
 
 export default function App() {
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
         <Route path='/about' element={<AboutPage />} />
+        <Route path='/services' element={<ServicesPage />} />
       </Route>
     </Routes>
   );
