e7da4caeb16b3147f906718144ccf5b1c5051295
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-17 19:48:29 -0500

    BUILD: Testimonial card

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ import HeroSection from "../../sections/HeroSection";
 import IntroSection from "../../sections/IntroSection";
 import ServicesSection from '../../sections/ServicesSection';
 import AboutCard from '../../components/AboutCard/AboutCard';
-
+import TestimonialSection from '../../sections/TestimonialSection';
 
 function LandingPage() {
 
@@ -17,6 +17,7 @@ function LandingPage() {
       <IntroSection />
       <AboutCard />
       <ServicesSection />
+      <TestimonialSection />
     </Box>
   );
 }
--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
+import { testimonials } from "../assets/testimonials";
+
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import TestimonialCard from "../components/TestimonialCard/TestimonialCard";
+
 export default function TestimonialSection() {
   return (
     <>
-    Testimonial Section
+      <Grid
+        id='testimonials'
+        component={'section'}
+        container
+        display={'flex'}
+        flexDirection={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        gap={10}
+        marginY={10}
+        
+      >
+        {testimonials.map((testimonial, index) => (
+          <Grid key={crypto.randomUUID()} >
+            <TestimonialCard  index={index} testimonial={testimonial}  onClick={() => handleTestimonialClick(testimonial)} />
+          </Grid>
+        ))}
+
+      </Grid>
       
     </>
   )
