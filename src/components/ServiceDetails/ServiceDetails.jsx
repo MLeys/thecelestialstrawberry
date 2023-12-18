f6f3b8cd517553e0751f0c01a6abc5b76cd8d465
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-18 07:23:58 -0500

    REFACT: service detail sizes

--- src/components/ServiceDetails/ServiceDetails.jsx
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -9,13 +9,13 @@ import ImageIcon from '../ImageIcon/ImageIcon';
 export default function ServiceDetails({ service, children}) {
 
   return (
-    <Paper component={Grid} container elevation={3} sx={{padding: 3, margin: 3}}>
+    <Paper component={Grid} container elevation={3} margin= {{xs: 1, sm: 3}} padding= {{xs: 1, sm: 3}} >
       <Grid xs={12}   >
         <Typography 
           variant='h4' color="primary.main" display={'flex'} align='center' alignItems={'center'} justifyContent={'flex-start'} gutterBottom
           sx={{fontSize: {xs: 'h4.fontSize', sm: 'h3.fontSize', md: 'h2.fontSize', lg: 'h1.fontSize'}}}
         >
-          <ImageIcon src={`symbols/${service.symbol}`} size={150} />
+          <ImageIcon src={`symbols/${service.symbol}`} size={{xs: 75, sm: 125, md: 150}} />
             {service.title}
         </Typography>
       </Grid>
