981241f7dac3d46ca11b023a325dedcad2a6aab7
MLeys <102929892+MLeys@users.noreply.github.com>
2023-12-17 19:35:29 -0500

    BUILD: sample testimonial data

---
+++ src/assets/testimonials.js
@@ -0,0 +1,21 @@
+
+// Sample information for testimonials
+export const testimonials = [
+  {
+    name: 'John Doe',
+    location: 'Grand Rapids, MI',
+    testimonial: ' I had a great experience with Laura. She was very professional and knowledgeable. I would recommend her to anyone looking for a enlightenment.'
+  },{
+    name: 'Jane Doe',
+    location: 'Alaska',
+    testimonial: ' I had a great experience with Laura. She was very professional and knowledgeable. I would recommend her to anyone looking for a enlightenment.'
+  },{
+    name: 'Becky Johnson',
+    location: 'Lakeland, FL',
+    testimonial: ' I had a great experience with Laura. She was very professional and knowledgeable. I would recommend her to anyone looking for a enlightenment.'
+  },{
+    name: 'Jabob Wilson',
+    location: 'Jersey City, NJ',
+    testimonial: ' I had a great experience with Laura. She was very professional and knowledgeable. I would recommend her to anyone looking for a enlightenment.'
+  }
+];
